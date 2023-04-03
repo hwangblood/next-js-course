577afbc799e5ad9e797052758e6a0d98dfe2a11f
hwangblood <hwangblood@gmail.com>
2023-04-03 18:52:35 +0800

    L03: Progressively Rendering a Page

--- next-posts/app/users/[userId]/page.tsx
+++ next-posts/app/users/[userId]/page.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { Suspense } from "react";
+import UserPosts from "./components/UserPosts";
 
 async function getUser(userId: number) {
   const res = await fetch(
@@ -30,13 +31,15 @@ export default async function UserPage(props: Props) {
   const userData: Promise<User> = getUser(userId);
   const userPostsData: Promise<Post[]> = getUserPosts(userId);
 
-  const [user, userPosts] = await Promise.all([userData, userPostsData]);
+  const user = await userData;
 
   return (
     <>
-      <h2>user: {user.name}</h2>
+      <h1>{user.name}</h1>
       <br />
-      <h3>posts: {userPosts.length}</h3>
+      <Suspense fallback={<h2>Loading...</h2>}>
+        <UserPosts promise={userPostsData} />
+      </Suspense>
     </>
   );
 }
