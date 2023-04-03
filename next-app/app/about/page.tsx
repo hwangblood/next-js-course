0c718abbd2822a8fe78db2bfbbc2c8b04cf71061
hwangblood <hwangblood@gmail.com>
2023-04-03 15:28:07 +0800

    L02: Error Boundaries

--- next-app/app/about/page.tsx
+++ next-app/app/about/page.tsx
@@ -4,6 +4,7 @@ import React from "react";
 type Props = {};
 
 export default function About({}: Props) {
+  // throw Error("Unknown error"); // Uncomment to show an error, and see how Error Handling works
   return (
     <>
       <h1>About</h1>
