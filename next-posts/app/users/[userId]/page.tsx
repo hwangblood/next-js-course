789171252f4e2f5a57b5f36aafc69c088187f717
hwangblood <hwangblood@gmail.com>
2023-04-03 18:36:42 +0800

    L03: Fetching Data in Parallel

--- next-posts/app/users/[userId]/page.tsx
+++ next-posts/app/users/[userId]/page.tsx
@@ -1,12 +1,42 @@
 import React from "react";
 
+async function getUser(userId: number) {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${userId}`
+  );
+  if (!res.ok) throw new Error(`Failed to fetch user by id ${userId}`);
+
+  return res.json();
+}
+
+async function getUserPosts(userId: number) {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?userId=${userId}`
+  );
+  if (!res.ok) throw new Error(`Failed to fetch posts of userId ${userId}`);
+
+  return res.json();
+}
+
 type Props = {
   params: {
     userId: number;
   };
 };
 
-export default function page(props: Props) {
+export default async function UserPage(props: Props) {
   const userId = props.params.userId;
-  return <div>{`user with id ${userId}`}</div>;
+
+  const userData: Promise<User> = getUser(userId);
+  const userPostsData: Promise<Post[]> = getUserPosts(userId);
+
+  const [user, userPosts] = await Promise.all([userData, userPostsData]);
+
+  return (
+    <>
+      <h2>user: {user.name}</h2>
+      <br />
+      <h3>posts: {userPosts.length}</h3>
+    </>
+  );
 }
