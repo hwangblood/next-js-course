09c86a8e2506e1393d04304ec711d45829c4abae
hwangblood <hwangblood@gmail.com>
2023-06-07 09:08:49 +0800

    L04: Segment level revalidation

--- next-posts/app/users/[userId]/page.tsx
+++ next-posts/app/users/[userId]/page.tsx
@@ -21,8 +21,10 @@ async function getUserPosts(userId: number) {
       // *but if data is changing constantly, we don't want always see the old data, for that can use dynamic caching
       // cache: "no-store",
 
-      // * ISR (incremental static regeneration), we want to go ahead and create the page and just check every so often to see if there's an update
+      // * ISR (incremental static regeneration), we want to go ahead and create the page and just check every so often to see if there's an update, https://nextjs.org/docs/app/building-your-application/data-fetching/fetching#revalidating-data
+
       next: {
+        // * Segment-level Caching https://nextjs.org/docs/app/building-your-application/data-fetching/caching#segment-level-caching
         revalidate: 60,
       },
     }
