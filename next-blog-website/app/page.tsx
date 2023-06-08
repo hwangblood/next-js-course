6cb36802d93c6c778f56558aa1608614b894e477
hwangblood <hwangblood@gmail.com>
2023-06-08 11:48:34 +0800

    L06: TailwindCSS Setup & Typography

--- next-blog-website/app/page.tsx
+++ next-blog-website/app/page.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
-    <main>
-      <h1>Hello</h1>
+    <main className="px-6 mx-auto">
+      <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
+        Hello and Welcome 👋&nbsp;
+        <span className="whitespace-nowrap">
+          I&apos;m <span className="font-bold">hwangblood</span>.
+        </span>
+      </p>
     </main>
   );
 }
