f74cd594c352ce717981dae03d24d640bcaa5c6a
hwangblood <hwangblood@gmail.com>
2023-06-08 15:05:33 +0800

    L06: Generate dynamic metadata

--- next-blog-website/app/posts/[postId]/page.tsx
+++ next-blog-website/app/posts/[postId]/page.tsx
@@ -1,6 +1,19 @@
 import { getSortedPostsData } from "@/lib/posts";
 import { notFound } from "next/navigation";
 
+export function generateMetadata({ params }: Props) {
+  const posts = getSortedPostsData(); // deduped!
+  const { postId } = params;
+
+  const post = posts.find((post) => post.id === postId);
+
+  if (!post) {
+    return { title: "Post Not Found" };
+  }
+
+  return { title: post.title };
+}
+
 type Props = {
   params: {
     postId: string;
@@ -8,7 +21,7 @@ type Props = {
 };
 
 export default function PostPage({ params }: Props) {
-  const posts = getSortedPostsData();
+  const posts = getSortedPostsData(); // deduped!
   const { postId } = params;
 
   if (!posts.find((post) => post.id === postId)) {
