7066e65fbd96d96e9e0549a3a5190f18c32be8c7
hwangblood <hwangblood@gmail.com>
2023-06-08 10:56:44 +0800

    L05: Results with the Item component

--- next-small-project/app/[searchTerm]/page.tsx
+++ next-small-project/app/[searchTerm]/page.tsx
@@ -1,4 +1,5 @@
 import getWikiResults from "@/lib/getWikiResults";
+import Item from "./components/Item";
 
 type Props = {
   params: { searchTerm: string };
@@ -28,8 +29,8 @@ export default async function SearchResults({ params: { searchTerm } }: Props) {
   const content = (
     <main className="bg-slate-200 mx-auto max-w-lg py-1 min-h-screen">
       {results ? (
-        Object.values(results).map((result, index) => {
-          return <p key={index}>{JSON.stringify(result)}</p>;
+        Object.values(results).map((result) => {
+          return <Item key={result.pageid} result={result} />;
         })
       ) : (
         <h2 className="p-2 text-xl">{`${searchTerm} Not Found!`}</h2>
