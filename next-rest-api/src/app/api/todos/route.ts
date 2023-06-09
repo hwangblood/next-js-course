ba573f23d7bc3bbcf64d17a8e6901a2d23e418fd
hwangblood <hwangblood@gmail.com>
2023-06-09 15:19:07 +0800

    L08: DELETE route handler

--- next-rest-api/src/app/api/todos/route.ts
+++ next-rest-api/src/app/api/todos/route.ts
@@ -1,6 +1,8 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 const DATA_SOURCE_URL = "http://jsonplaceholder.typicode.com/todos";
+
+const API_KEY: string = process.env.DATA_API_KEY as string;
 
 export async function GET(request: Request) {
   const res = await fetch(DATA_SOURCE_URL);
@@ -8,4 +10,22 @@ export async function GET(request: Request) {
   const todos: Todo[] = await res.json();
 
   return NextResponse.json(todos);
+}
+
+export async function DELETE(request: Request) {
+  // ! error SyntaxError: Unexpected end of JSON input https://github.com/vercel/next.js/issues/48096
+  const { id }: Partial<Todo> = await request.json(); // error
+
+  if (!id) return NextResponse.json({ message: "Todo id required" });
+
+  await fetch(`${DATA_SOURCE_URL}/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      // * this "API-Key" is not required for jsonplaceholder, it just a demo
+      "API-Key": API_KEY,
+    },
+  });
+
+  return NextResponse.json({ message: `Todo ${id} deleted successfully` });
 }
