37fd4731ce961c026bdb296bbfc780eca9d0c10d
hwangblood <hwangblood@gmail.com>
2023-06-09 15:26:34 +0800

    L08: POST route handler

--- next-rest-api/src/app/api/todos/route.ts
+++ next-rest-api/src/app/api/todos/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
 const DATA_SOURCE_URL = "http://jsonplaceholder.typicode.com/todos";
 
@@ -28,4 +28,28 @@ export async function DELETE(request: Request) {
   });
 
   return NextResponse.json({ message: `Todo ${id} deleted successfully` });
+}
+
+export async function POST(request: Request) {
+  const { userId, title }: Partial<Todo> = await request.json();
+
+  if (!userId || !title)
+    return NextResponse.json({ message: "Missing required data" });
+
+  const res = await fetch(DATA_SOURCE_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "API-Key": API_KEY,
+    },
+    body: JSON.stringify({
+      userId,
+      title,
+      completed: false,
+    }),
+  });
+
+  const newTodo: Todo = await res.json();
+
+  return NextResponse.json(newTodo);
 }
