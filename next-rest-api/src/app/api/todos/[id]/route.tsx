762b6d406996b1ada655df399ef6f442d4b4d5a6
hwangblood <hwangblood@gmail.com>
2023-06-10 12:30:14 +0800

    L09: Quick follow-up on last lesson

--- next-rest-api/src/app/api/todos/[id]/route.tsx
+++ next-rest-api/src/app/api/todos/[id]/route.tsx
@@ -2,10 +2,16 @@ import { NextResponse } from "next/server";
 
 const DATA_SOURCE_URL = "https://jsonplaceholder.typicode.com/todos";
 
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
 // http://localhost:3000/api/todos/23
 // http://localhost:3000/api/todos/2234  404 Not Found
-export async function GET(request: Request) {
-  const id = request.url.slice(request.url.lastIndexOf("/") + 1);
+export async function GET(request: Request, { params: { id } }: Props) {
+  // const id = request.url.slice(request.url.lastIndexOf("/") + 1);
 
   const res = await fetch(`${DATA_SOURCE_URL}/${id}`);
 
