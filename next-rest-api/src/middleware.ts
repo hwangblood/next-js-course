60e5ce99d81515a1b4cf672beab4abdea8441511
hwangblood <hwangblood@gmail.com>
2023-06-10 14:43:17 +0800

    L09: Cors & Next.js 13

--- next-rest-api/src/middleware.ts
+++ next-rest-api/src/middleware.ts
@@ -5,9 +5,25 @@ import type { NextRequest } from "next/server";
  * Routing: Middleware | Next.js
  * https://nextjs.org/docs/app/building-your-application/routing/middleware
  */
+const allowedOrigins =
+  process.env.NODE_ENV === "production"
+    ? ["http://www.yoursite.com", "http://yoursite.com"]
+    : ["http://localhost:3000", "https://www.google.com"];
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
+  const origin = request.headers.get("origin");
+  // If the origin is not allowed, or no origin (postman, thunderclient)
+  if ((origin && !allowedOrigins.includes(origin)) || !origin) {
+    return new NextResponse(null, {
+      status: 400,
+      statusText: "Bad Request",
+      headers: {
+        "Content-Type": "text/plain",
+      },
+    });
+  }
+
   const regex = new RegExp("/api/todos/*");
   // check whether not match the regex pattern, /api/hello
   if (!regex.test(request.url)) {
@@ -17,9 +33,6 @@ export function middleware(request: NextRequest) {
   console.log("Middleware!");
   console.log(request.method);
   console.log(request.url);
-
-  const origin = request.headers.get("origin");
-  console.log(`origin: ${origin}`);
 
   const userAgent = request.headers.get("User-Agent");
   console.log(`User-Agent: ${userAgent}`);
